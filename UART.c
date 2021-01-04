261d7d0cfa81220f62322a8c381d140e2044cbf6
Aleksa Stojkovic <alexstoj.ns@gmail.com>
2021-01-04 12:40:03 +0100

    Fajlovi za projekat

---
+++ UART.c
@@ -0,0 +1,36 @@
+#include <p30fxxxx.h>
+#include "UART.h"
+
+void initUART1(void)
+{
+    U1BRG=0x0040;//ovim odredjujemo baudrate
+
+    U1MODEbits.ALTIO=0;//biramo koje pinove koristimo za komunikaciju osnovne ili alternativne
+
+    IEC0bits.U1RXIE=1;//omogucavamo rx1 interupt
+
+    U1STA&=0xfffc;
+
+    U1MODEbits.UARTEN=1;//ukljucujemo ovaj modul
+
+    U1STAbits.UTXEN=1;//ukljucujemo predaju
+}
+
+void RS232_putstr(register const char *str)
+{
+    while((*str) != 0)
+    {
+        WriteUART1(*str);
+        str++;
+    }
+}
+
+void WriteUART1(unsigned int data)
+{
+	while(!U1STAbits.TRMT);
+
+    if(U1MODEbits.PDSEL == 3)
+        U1TXREG = data;
+    else
+        U1TXREG = data & 0xFF;
+}
---
+++ UART.h
@@ -0,0 +1,5 @@
+#include <p30fxxxx.h>
+
+void initUART1(void);
+void RS232_putstr(register const char *str);
+void WriteUART1(unsigned int data);
